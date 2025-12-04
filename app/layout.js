0dadf200c8af5b0c5050d1012f5ffa910c0ea831
conlele0201 <129933655+conlele0201@users.noreply.github.com>
2025-12-04 13:31:27 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,11 @@
-// app/layout.js
-
-import './globals.css';
-
 export const metadata = {
-  title: 'ERP SELLER – Base',
-  description: 'ERP SELLER CRM base layout',
+  title: "ERP SELLER",
+  description: "ERP Seller CRM",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="vi">
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
