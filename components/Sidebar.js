6145757d49b677d5ef70b6b13f1ba175379f14b2
conlele0201 <129933655+conlele0201@users.noreply.github.com>
2025-12-04 14:50:29 +0700

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,1 +1,18 @@
 
+export default function Sidebar() {
+  return (
+    <div
+      style={{
+        width: "200px",
+        padding: "20px",
+        backgroundColor: "#f3f4f6",
+        borderRight: "1px solid #ccc",
+        height: "100vh"
+      }}
+    >
+      <h3>Sidebar Test</h3>
+      <p>Menu 1</p>
+      <p>Menu 2</p>
+    </div>
+  );
+}
