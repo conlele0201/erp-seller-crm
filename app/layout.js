ab416c932ebc3dc392a8da2994365a275522ab61
conlele0201 <129933655+conlele0201@users.noreply.github.com>
2025-12-04 14:16:29 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,27 @@
+import "./globals.css";
+import Sidebar from "@/components/Sidebar";
+
 export const metadata = {
   title: "ERP SELLER",
-  description: "ERP Seller CRM",
+  description: "Quản lý hệ thống ERP Seller",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="vi">
-      <body style={{ margin: 0, padding: 0 }}>{children}</body>
+    <html lang="en">
+      <body>
+        <div style={{ display: "flex", height: "100vh" }}>
+          {/* Sidebar trái */}
+          <Sidebar />
+
+          {/* Nội dung bên phải */}
+          <div style={{ flex: 1, padding: "20px" }}>
+            <h2 style={{ fontWeight: "600", fontSize: "20px" }}>ERP SELLER</h2>
+            <hr style={{ margin: "20px 0" }} />
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
