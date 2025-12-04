08971908c5c501a38b54c6c0cadfbe299e6135ef
conlele0201 <129933655+conlele0201@users.noreply.github.com>
2025-12-04 07:49:50 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,11 @@ export default function RootLayout({ children }) {
     <html lang="vi">
       <body>
         <Sidebar />
-        {children}
+
+        {/* Khung nội dung dịch sang phải 260px */}
+        <div style={{ marginLeft: "260px", padding: "20px" }}>
+          {children}
+        </div>
       </body>
     </html>
   );
