e19d46a3ae3e5b9f88456d189615b3c876e5e917
conlele0201 <129933655+conlele0201@users.noreply.github.com>
2025-12-04 08:22:41 +0700

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,6 +1,17 @@
 export default function Sidebar() {
   return (
-    <div>
+    <div
+      style={{
+        width: "260px",
+        backgroundColor: "#f3f4f6",
+        height: "100vh",
+        padding: "20px",
+        borderRight: "1px solid #e5e7eb",
+        position: "fixed",
+        top: 0,
+        left: 0
+      }}
+    >
       Sidebar ERP SELLER
     </div>
   );
